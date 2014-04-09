6d9030dfc2b8440034dd3cbea86b66ca41c966ba
Eguzki Astiz Lezaun <eguzki@tid.es>
2014-04-09 13:12:20 +0200

    simple example

--- main.cpp
+++ main.cpp
@@ -1,12 +1,11 @@
+#include "mainwindow.h"
 #include <QApplication>
-#include <QLabel>
-#include <QWidget>
-int main(int argc, char *argv[ ])
+
+int main(int argc, char *argv[])
 {
-    QApplication app(argc, argv);
-    QLabel hello("<center>Urko se descarga porno afgano everyday</center>");
-    hello.setWindowTitle("Fucking windows tittle");
-    hello.resize(400, 400);
-    hello.show();
-    return app.exec();
+    QApplication a(argc, argv);
+    MainWindow w;
+    w.show();
+
+    return a.exec();
 }
