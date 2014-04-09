ea979100a6a7fe3e98a3f94745ed70979f03f71f
Eguzki Astiz Lezaun <eguzki@tid.es>
2014-04-09 12:07:30 +0200

    initial commit

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include <QApplication>
+#include <QLabel>
+#include <QWidget>
+int main(int argc, char *argv[ ])
+{
+    QApplication app(argc, argv);
+    QLabel hello("<center>Urko se descarga porno afgano everyday</center>");
+    hello.setWindowTitle("Fucking windows tittle");
+    hello.resize(400, 400);
+    hello.show();
+    return app.exec();
+}
