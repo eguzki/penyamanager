a64de6a7dd3cbcc369b611bc24fbfecb95691ab5
Urko <Urko@Urko-PC>
2014-11-14 17:25:53 +0100

    Minor changes.

--- penyamanager/mainwindow.cpp
+++ penyamanager/mainwindow.cpp
@@ -6,6 +6,7 @@ MainWindow::MainWindow(QWidget *parent) :
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+
     this->connect(this->ui->pushButton, SIGNAL(clicked()), this, SLOT(pushbutton1_click()));
 }
 
