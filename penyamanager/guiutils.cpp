9d97b08be878faa3ce7908632a2f3aca0c39c2a1
Eguzki Astiz Lezaun <eguzki@tid.es>
2015-10-31 15:52:54 +0100

    deposit print

--- penyamanager/guiutils.cpp
+++ penyamanager/guiutils.cpp
@@ -22,32 +22,34 @@ namespace PenyaManager {
     void GuiUtils::printText(const QString& html)
     {
         /*
+         * PDF
+        //QPrinter printer(QPrinter::HighResolution);
+        //printer.setOutputFormat(QPrinter::PdfFormat);
+        //printer.setOutputFileName("deposit.pdf");
+        */
+        QSizeF paperSize(80, 297);
         QPrinterInfo defaultPrinter = QPrinterInfo::defaultPrinter();
         if (defaultPrinter.isNull()) {
             QLOG_INFO() << QString("[PrintFailed] default printer is null");
             return;
         }
-        */
-        QPrinter printer(QPrinter::HighResolution);
-        printer.setOutputFormat(QPrinter::PdfFormat);
-        printer.setOutputFileName("deposit.pdf");
-        printer.setPaperSize(QPrinter::A6);
-        //printer.setPageSize(QPrinter::A6);
+        QPrinter printer( defaultPrinter);
+        printer.setPaperSize(paperSize, QPrinter::Millimeter);
         if (!printer.isValid()) {
             QLOG_ERROR() << QString("[PrintFailed] pdf printer not valid");
             return;
         }
+
         // nedeed to keep printer from printing page number
-        QSizeF paperSize;
-        paperSize.setWidth(printer.pageRect(QPrinter::Millimeter).width());
-        paperSize.setHeight(printer.pageRect(QPrinter::Millimeter).height());
+        QSizeF pageSize;
+        pageSize.setWidth(printer.pageRect(QPrinter::Millimeter).width());
+        pageSize.setHeight(printer.pageRect(QPrinter::Millimeter).height());
         QTextDocument textDocument;
         // supported attributes
         // http://doc.qt.io/qt-5/richtext-html-subset.html
         // Use tables for layout
-        textDocument.setPageSize(paperSize); // the document needs a valid PageSize
+        textDocument.setPageSize(pageSize); // the document needs a valid PageSize
         textDocument.setHtml(html);
         textDocument.print(&printer);
-
     }
 }
