3bfa702036e8a7b9a248f1dca7a7a394a5e3e781
Eguzki Astiz Lezaun <eguzki.astiz@gmail.com>
2017-07-12 22:07:37 +0200

    product item widget

--- penyamanager/commons/productitemwidget.h
+++ penyamanager/commons/productitemwidget.h
@@ -2,21 +2,28 @@
 #define PRODUCTITEMWIDGET_H
 
 #include <QWidget>
+#include <commons/DataTypes.h>
 
 namespace Ui {
 class ProductItemWidget;
 }
 
-class ProductItemWidget : public QWidget
-{
-    Q_OBJECT
+namespace PenyaManager {
+    //
+    class ProductItemWidget : public QWidget
+    {
+        Q_OBJECT
 
-public:
-    explicit ProductItemWidget(QWidget *parent = 0);
-    ~ProductItemWidget();
+        public:
+            //
+            ProductItemWidget(QWidget *parent, const QPixmap &imageMap, const QString &text, Float price);
+            //
+            ~ProductItemWidget();
 
-private:
-    Ui::ProductItemWidget *ui;
-};
+        private:
+            //
+            Ui::ProductItemWidget *ui;
+    };
+}
 
 #endif // PRODUCTITEMWIDGET_H
